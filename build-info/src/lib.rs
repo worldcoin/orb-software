aadd57e56e1ecb71ab1d90e1aba6cae01ea5af69
Ryan Butler <thebutlah@gmail.com>
2023-12-20 03:29:43 -0600

    Hotfix to ensure build-info is accurate
    * Hotfix to ensure build-info is accurate
    
    * Update zerocopy for rustsec advisory
    
    * foobar

--- build-info/src/lib.rs
+++ build-info/src/lib.rs
@@ -1,14 +1,13 @@
+//! Be sure that you run [`build_info_helper::initialize()`] in your build.rs.
 #![no_std]
+
+#[cfg(feature = "build-script")]
+pub use build_info_helper::*;
 
 // Must be the same as the one in build.rs
 macro_rules! prefix_env {
     ($var:literal) => {
-        env!(concat!(
-            "WORLDCOIN_BUILD_INFO_",
-            env!("CARGO_PKG_VERSION"),
-            "_",
-            $var
-        ))
+        env!(concat!("WORLDCOIN_BUILD_INFO_", $var))
     };
 }
 
