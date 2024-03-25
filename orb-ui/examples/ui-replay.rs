cb2d82d88fe2b3e19a6ed3c37c3fa7307d92c0d6
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2024-03-25 09:16:06 +0100

    orb-ui: use from_str to parse orb-ui's logs
    logs from journalctl

--- orb-ui/examples/ui-replay.rs
+++ orb-ui/examples/ui-replay.rs
@@ -1,9 +1,10 @@
 use chrono::{DateTime, Utc};
 use clap::Parser;
-use eyre::{Context, Result};
+use eyre::{Context, ContextCompat, Result};
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use std::str::FromStr;
 use tokio::time::sleep;
 use tracing::level_filters::LevelFilter;
 use tracing::{debug, info};
@@ -23,23 +24,6 @@ trait SignupState {
     fn orb_signup_state_event(&self, serialized_event: String) -> zbus::Result<String>;
 }
 
-fn parse_line(line: &str) -> Option<(DateTime<Utc>, &str)> {
-    let parts: Vec<&str> = line.split(' ').collect();
-    if parts.len() < 2 {
-        return None;
-    }
-    let timestamp_str = parts[5];
-
-    // split line to take everything after "UI event:"
-    let (_, event) = line.split_once("UI event: ")?;
-    debug!("Timestamp: {}, Event: {}", timestamp_str, event);
-
-    match timestamp_str.parse::<DateTime<Utc>>() {
-        Ok(timestamp) => Some((timestamp, event)),
-        Err(_) => None,
-    }
-}
-
 /// Utility args
 #[derive(clap::Parser, Debug)]
 #[clap(
@@ -51,6 +35,37 @@ fn parse_line(line: &str) -> Option<(DateTime<Utc>, &str)> {
 struct Args {
     #[clap(short, long)]
     path: Option<String>,
+}
+
+#[derive(Debug, Default)]
+struct EventRecord {
+    timestamp: DateTime<Utc>,
+    event: String,
+}
+
+impl FromStr for EventRecord {
+    type Err = eyre::Report;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = line.split(' ').collect();
+        if parts.len() < 2 {
+            return Err(eyre::eyre!("Line is too short"));
+        }
+        let timestamp_str = parts[5];
+
+        // split line to take everything after "UI event:"
+        let (_, event) = line
+            .split_once("UI event: ")
+            .wrap_err("Unable to split line")?;
+        let event = event.to_string();
+        match timestamp_str.parse::<DateTime<Utc>>() {
+            Ok(timestamp) => {
+                debug!("Timestamp: {:?}, Event: {:?}", timestamp, event);
+                Ok(EventRecord { timestamp, event })
+            }
+            Err(error) => Err(eyre::eyre!("Unable to parse timestamp: {error}")),
+        }
+    }
 }
 
 #[tokio::main]
@@ -81,20 +96,23 @@ async fn main() -> Result<()> {
     let reader = io::BufReader::new(file);
 
     let mut last_timestamp: Option<DateTime<Utc>> = None;
-    for line in reader.lines() {
-        let line = line?;
-        if let Some((timestamp, event)) = parse_line(&line) {
-            if let Some(last) = last_timestamp {
-                let delay = timestamp - last;
-                sleep(delay.to_std().unwrap()).await;
-            }
-
-            info!("Sending: {}", event);
-            // send the event to orb-ui over dbus
-            let _ = proxy.orb_signup_state_event(format!("\"{event}\"")).await;
-
-            last_timestamp = Some(timestamp);
+    for record in reader
+        .lines()
+        .filter_map(|r| r.ok())
+        .map(|line| line.parse::<EventRecord>())
+    {
+        let record = record?;
+        if let Some(last) = last_timestamp {
+            let delay = record.timestamp - last;
+            sleep(delay.to_std().unwrap()).await;
         }
+
+        let event = record.event;
+        info!("Sending: {}", event);
+        // send the event to orb-ui over dbus
+        let _ = proxy.orb_signup_state_event(format!("\"{event}\"")).await;
+
+        last_timestamp = Some(record.timestamp);
     }
 
     Ok(())
