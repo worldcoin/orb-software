d85431844e6502a8997d9209a3b3eda01d036790
Ryan Butler <thebutlah@gmail.com>
2024-05-14 06:29:55 -0400

    hil: initial commit (#97)
    Adds an orb-hil binary, to make flashing easy and automated. Its little
    more than a glorified bash script right now.
    
    Currently it:
    * Assumes that it is on the hil and relies on some cli tools provided
    from the nix shell, like awscli and tar
    * Supports flashing from a s3 url or a local tarball
    
    It does not yet support:
    * Automatically controlling CTS and RTS pins to reboot orb or trigger
    recovery mode
    * Connecting to serial
    * Retrieving s3 urls from a git tag
    * Automatically running in response to network events or github runners

---
+++ hil/src/flash.rs
@@ -0,0 +1,77 @@
+use std::path::Path;
+
+use camino::Utf8Path;
+use cmd_lib::run_cmd;
+use color_eyre::{
+    eyre::{ensure, WrapErr},
+    Result, Section,
+};
+use tempfile::TempDir;
+
+pub async fn flash(variant: FlashVariant, path_to_rts_tar: &Utf8Path) -> Result<()> {
+    let path_to_rts = path_to_rts_tar.to_owned();
+    tokio::task::spawn_blocking(move || {
+        let tmp_dir = extract(&path_to_rts)?;
+        println!("{tmp_dir:?}");
+        flash_cmd(variant, tmp_dir.path())?;
+        Ok(())
+    })
+    .await
+    .wrap_err("task panicked")?
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum FlashVariant {
+    Fast,
+    Regular,
+}
+
+impl FlashVariant {
+    fn file_name(&self) -> &'static str {
+        match self {
+            FlashVariant::Fast => "fastflashcmd.txt",
+            FlashVariant::Regular => "flashcmd.txt",
+        }
+    }
+}
+
+fn extract(path_to_rts: &Utf8Path) -> Result<TempDir> {
+    ensure!(
+        path_to_rts.try_exists().unwrap_or(false),
+        "{path_to_rts} doesn't exist"
+    );
+    ensure!(path_to_rts.is_file(), "{path_to_rts} should be a file!");
+    let temp_dir = TempDir::new_in(path_to_rts.parent().unwrap())
+        .wrap_err("failed to create temporary extract dir")?;
+    let extract_dir = temp_dir.path();
+    let result = run_cmd! {
+        cd $extract_dir;
+        info extracting rts $path_to_rts;
+        tar xvf $path_to_rts;
+        info finished extract!;
+    };
+    result
+        .wrap_err("failed to extract rts")
+        .with_note(|| format!("path_to_rts was {path_to_rts}"))?;
+    Ok(temp_dir)
+}
+
+fn flash_cmd(variant: FlashVariant, extracted_dir: &Path) -> Result<()> {
+    let bootloader_dir = extracted_dir.join("ready-to-sign").join("bootloader");
+    ensure!(
+        bootloader_dir.try_exists().unwrap_or(false),
+        "{bootloader_dir:?} doesn't exist"
+    );
+
+    let cmd_file_name = variant.file_name();
+    let result = run_cmd! {
+        cd $bootloader_dir;
+        info running $cmd_file_name;
+        bash $cmd_file_name;
+        info finished flashing!;
+    };
+    result
+        .wrap_err("failed to flash rts")
+        .with_note(|| format!("bootloader_dir was {bootloader_dir:?}"))?;
+    Ok(())
+}
---
+++ hil/src/main.rs
@@ -0,0 +1,102 @@
+#![forbid(unsafe_code)]
+
+mod download_s3;
+mod flash;
+
+use camino::Utf8PathBuf;
+use clap::Parser;
+use color_eyre::{
+    eyre::{bail, WrapErr},
+    Result,
+};
+use flash::FlashVariant;
+use orb_build_info::{make_build_info, BuildInfo};
+use tracing::info;
+use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
+
+const BUILD_INFO: BuildInfo = make_build_info!();
+
+#[derive(Parser, Debug)]
+#[command(about, author, version=BUILD_INFO.git.describe, styles=make_clap_v3_styles())]
+struct Cli {
+    /// The s3 URI of the rts.
+    #[arg(long)]
+    s3_url: Option<String>,
+    /// The directory to save the s3 artifact we download.
+    #[arg(long)]
+    download_dir: Option<Utf8PathBuf>,
+    /// Skips download by using an existing tarball on the filesystem.
+    #[arg(long)]
+    rts_path: Option<Utf8PathBuf>,
+    /// if this flag is given, uses flashcmd.txt instead of fastflashcmd.txt
+    #[arg(long)]
+    slow: bool,
+}
+
+fn current_dir() -> Utf8PathBuf {
+    std::env::current_dir().unwrap().try_into().unwrap()
+}
+
+// No need to waste RAM with a threadpool.
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    let args = Cli::parse();
+
+    let rts_path = if let Some(ref s3_url) = args.s3_url {
+        if args.rts_path.is_some() {
+            bail!("both rts_path and s3_url were specified - only provide one or the other");
+        }
+        let download_dir = args.download_dir.unwrap_or(current_dir());
+        let download_path = download_dir.join(
+            crate::download_s3::parse_filename(s3_url)
+                .wrap_err("failed to parse filename")?,
+        );
+
+        crate::download_s3::download_url(s3_url, &download_path)
+            .await
+            .wrap_err("error while downloading from s3")?;
+
+        download_path
+    } else if let Some(rts_path) = args.rts_path {
+        if args.s3_url.is_some() {
+            bail!("both rts_path and s3_url were specified - only provide one or the other");
+        }
+        if args.download_dir.is_some() {
+            bail!("both rts_path and download_dir were specified - only provide one or the other");
+        }
+        info!("using already downloaded rts tarball");
+        rts_path
+    } else {
+        bail!("you must provide either rts_path or s3_url");
+    };
+
+    let variant = if args.slow {
+        FlashVariant::Regular
+    } else {
+        FlashVariant::Fast
+    };
+    crate::flash::flash(variant, &rts_path)
+        .await
+        .wrap_err("error while flashing")?;
+
+    Ok(())
+}
+
+fn make_clap_v3_styles() -> clap::builder::Styles {
+    use clap::builder::styling::AnsiColor;
+    clap::builder::Styles::styled()
+        .header(AnsiColor::Yellow.on_default())
+        .usage(AnsiColor::Green.on_default())
+        .literal(AnsiColor::Green.on_default())
+        .placeholder(AnsiColor::Green.on_default())
+}
