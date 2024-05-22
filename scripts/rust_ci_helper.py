a4fab3b66ec211bf3f2ebfc6ee5c69f75a635656
Ryan Butler <thebutlah@gmail.com>
2024-05-22 17:55:02 -0400

    reorder imports

--- scripts/rust_ci_helper.py
+++ scripts/rust_ci_helper.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import json
-import argparse
-import subprocess
-import os
 from collections import defaultdict
+
+import argparse
+import json
+import os
+import subprocess
 import sys
 
 
