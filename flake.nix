2d74f50ee23189acb1901422f85e179564e15ee0
Ryan Butler <thebutlah@gmail.com>
2024-05-29 15:27:37 -0400

    nix: switch to nixpkgs-23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "orb-software flake";
   inputs = {
     # Worlds largest repository of linux software
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     # Provides eachDefaultSystem and other utility functions
     utils.url = "github:numtide/flake-utils";
     # Replacement for rustup
