fdb2ec0a9b2196800c9578ac8403e722b99062ea
Ryan Butler <thebutlah@gmail.com>
2024-07-16 19:11:16 -0400

    hil: added ftdi reboot support

--- hil/src/boot.rs
+++ hil/src/boot.rs
@@ -1,4 +1,12 @@
+use std::time::Duration;
+
+use crate::ftdi::{FtdiGpio, OutputState};
 use cmd_lib::run_cmd;
+use color_eyre::{eyre::WrapErr as _, Result};
+use tracing::info;
+
+const POWER_PIN: crate::ftdi::Pin = FtdiGpio::CTS_PIN;
+const RECOVERY_PIN: crate::ftdi::Pin = FtdiGpio::RTS_PIN;
 
 pub fn is_recovery_mode_detected() -> bool {
     run_cmd! {
@@ -6,4 +14,51 @@ pub fn is_recovery_mode_detected() -> bool {
         lsusb | rg "NVIDIA Corp. APX";
     }
     .is_ok()
+}
+
+// Note: we are calling some blocking code from async here, but its probably fine.
+#[tracing::instrument]
+pub async fn reboot(recovery: bool) -> Result<()> {
+    fn make_ftdi() -> Result<FtdiGpio> {
+        FtdiGpio::builder()
+            .with_default_device()
+            .and_then(|b| b.configure())
+            .wrap_err("failed to create ftdi device")
+    }
+    info!("Turning off");
+    let ftdi = tokio::task::spawn_blocking(|| -> Result<_, color_eyre::Report> {
+        let mut ftdi = make_ftdi()?;
+        ftdi.set_pin(POWER_PIN, OutputState::Low)?;
+        ftdi.set_pin(RECOVERY_PIN, OutputState::High)?;
+        Ok(ftdi)
+    })
+    .await
+    .wrap_err("task panicked")??;
+    tokio::time::sleep(Duration::from_secs(10)).await;
+
+    info!("Resetting FTDI");
+    ftdi.destroy().wrap_err("failed to destroy ftdi")?;
+    tokio::time::sleep(Duration::from_secs(4)).await;
+
+    info!("Turning on");
+    let ftdi = tokio::task::spawn_blocking(move || -> Result<_, color_eyre::Report> {
+        let mut ftdi = make_ftdi()?;
+        let recovery_state = if recovery {
+            OutputState::Low
+        } else {
+            OutputState::High
+        };
+        ftdi.set_pin(POWER_PIN, OutputState::Low)?;
+        ftdi.set_pin(RECOVERY_PIN, recovery_state)?;
+        Ok(ftdi)
+    })
+    .await
+    .wrap_err("task panicked")??;
+    tokio::time::sleep(Duration::from_secs(4)).await;
+
+    ftdi.destroy().wrap_err("failed to destroy ftdi")?;
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    info!("Done");
+
+    Ok(())
 }
