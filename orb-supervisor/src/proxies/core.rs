78c4270b720639ae972fa44f774b5ead54240d54
Ryan Butler <thebutlah@gmail.com>
2024-09-25 14:31:52 -0400

    refactor: use zbus 4, migrate from deprecated macros (#227)

--- orb-supervisor/src/proxies/core.rs
+++ orb-supervisor/src/proxies/core.rs
@@ -1,17 +1,17 @@
 //! Dbus proxies for interacting with orb core.
 
-use zbus::dbus_proxy;
+use zbus::proxy;
 
 pub const SIGNUP_PROXY_DEFAULT_WELL_KNOWN_NAME: &str = "org.worldcoin.OrbCore1";
 pub const SIGNUP_PROXY_DEFAULT_OBJECT_PATH: &str = "/org/worldcoin/OrbCore1/Signup";
 
-#[dbus_proxy(
+#[proxy(
     interface = "org.worldcoin.OrbCore1.Signup",
     gen_blocking = false,
     default_service = "org.worldcoin.OrbCore1",
     default_path = "/org/worldcoin/OrbCore1/Signup"
 )]
 pub trait Signup {
-    #[dbus_proxy(signal)]
+    #[zbus(signal)]
     fn signup_started(&self) -> Result<()>;
 }
