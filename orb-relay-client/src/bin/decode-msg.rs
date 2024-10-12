77fecbb56999def522402cd476ec944a32a0ffda
Anastasios Andronidis <anastasios.andronidis@toolsforhumanity.com>
2024-10-13 01:25:11 +0300

    Decode msgs for type Any to known types (#1353)
    Decode msgs for type Any to known types

---
+++ orb-relay-client/src/bin/decode-msg.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+use eyre::{eyre, Result};
+use orb_relay_client::debug_any;
+use orb_relay_messages::prost_types::Any;
+use serde_json::Value;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg()]
+    json: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    println!("{}", decode_payload(&args.json)?);
+    Ok(())
+}
+
+fn decode_payload(json: &str) -> Result<String> {
+    println!("json: {}", json);
+    let v: Value = json5::from_str(json)?;
+    let any = Any {
+        type_url: v["type_url"].as_str().ok_or_else(|| eyre!("Invalid type_url"))?.to_string(),
+        value: v["value"]
+            .as_array()
+            .ok_or_else(|| eyre!("Invalid value"))?
+            .iter()
+            .map(|n| n.as_u64().ok_or_else(|| eyre!("Invalid number")).map(|n| n as u8))
+            .collect::<Result<_>>()?,
+    };
+    Ok(debug_any(&Some(any)))
+}
