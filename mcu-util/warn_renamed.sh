a14046eafd6c4df9f40cc78cf955d336cea77034
Ryan Butler <thebutlah@gmail.com>
2024-12-09 10:42:24 -0600

    fix mixed spaces

--- mcu-util/warn_renamed.sh
+++ mcu-util/warn_renamed.sh
@@ -6,7 +6,7 @@ PROGRAM="orb-mcu-util"
 
 if [ -t 0 ] ; then
     # Interactive terminal, print a warning
-	echo "$(tput setaf 1)WARNING: this program has been moved to /usr/local/bin/${PROGRAM}, this wrapper will be removed in the next release$(tput sgr0)" >&2
+    echo "$(tput setaf 1)WARNING: this program has been moved to /usr/local/bin/${PROGRAM}, this wrapper will be removed in the next release$(tput sgr0)" >&2
 else
     PARENT=$(cat /proc/$PPID/cmdline | tr \\0 \ )
     echo "moved to /usr/local/bin/${PROGRAM}" | systemd-cat -t ${PROGRAM} -p warning
