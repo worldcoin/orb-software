998fc03858c189ee4df77626c081b62d210aa3ce
Valentyn Valiaiev <valentine.valyaeff@gmail.com>
2024-12-12 20:43:07 +0200

    feat(orb-endpoints): Add a new Analysis backend (#329)
    Add Backend::Analysis

--- endpoints/src/endpoints.rs
+++ endpoints/src/endpoints.rs
@@ -18,9 +18,10 @@ impl Endpoints {
     /// # Errors
     /// Errors if the `orb_id` would result in an invalid URL.
     pub fn new(backend: Backend, orb_id: &OrbId) -> Self {
-        let maybe_stage = match backend {
-            Backend::Prod => "",
-            Backend::Staging => "stage.",
+        let subdomain = match backend {
+            Backend::Prod => "orb",
+            Backend::Staging => "stage.orb",
+            Backend::Analysis => "analysis.ml",
         };
 
         /// Safer way to assemble URLs involving `OrbId`
@@ -33,20 +34,16 @@ impl Endpoints {
 
         Self {
             ai_volume: concat_urls(
-                &format!(
-                    "https://management.{maybe_stage}orb.worldcoin.org/api/v1/orbs/"
-                ),
+                &format!("https://management.{subdomain}.worldcoin.org/api/v1/orbs/"),
                 orb_id,
                 "keys/aivolume",
             ),
             auth: Url::parse(&format!(
-                "https://auth.{maybe_stage}orb.worldcoin.org/api/v1/"
+                "https://auth.{subdomain}.worldcoin.org/api/v1/"
             ))
             .expect("urls with validated orb ids should always parse"),
             ping: concat_urls(
-                &format!(
-                    "https://management.{maybe_stage}orb.worldcoin.org/api/v1/orbs/"
-                ),
+                &format!("https://management.{subdomain}.worldcoin.org/api/v1/orbs/"),
                 orb_id,
                 "",
             ),
@@ -62,6 +59,7 @@ mod test {
     fn test_config() {
         let stage = Endpoints::new(Backend::Staging, &"ea2ea744".parse().unwrap());
         let prod = Endpoints::new(Backend::Prod, &"ea2ea744".parse().unwrap());
+        let analysis = Endpoints::new(Backend::Analysis, &"ea2ea744".parse().unwrap());
 
         assert_eq!(
             stage.ai_volume.as_str(),
@@ -71,12 +69,20 @@ mod test {
             prod.ai_volume.as_str(),
             "https://management.orb.worldcoin.org/api/v1/orbs/ea2ea744/keys/aivolume"
         );
+        assert_eq!(
+            analysis.ai_volume.as_str(),
+            "https://management.analysis.ml.worldcoin.org/api/v1/orbs/ea2ea744/keys/aivolume"
+        );
 
         assert_eq!(
             stage.auth.as_str(),
             "https://auth.stage.orb.worldcoin.org/api/v1/"
         );
         assert_eq!(prod.auth.as_str(), "https://auth.orb.worldcoin.org/api/v1/");
+        assert_eq!(
+            analysis.auth.as_str(),
+            "https://auth.analysis.ml.worldcoin.org/api/v1/"
+        );
 
         assert_eq!(
             stage.ping.as_str(),
@@ -85,6 +91,10 @@ mod test {
         assert_eq!(
             prod.ping.as_str(),
             "https://management.orb.worldcoin.org/api/v1/orbs/ea2ea744/"
+        );
+        assert_eq!(
+            analysis.ping.as_str(),
+            "https://management.analysis.ml.worldcoin.org/api/v1/orbs/ea2ea744/"
         );
     }
 }
