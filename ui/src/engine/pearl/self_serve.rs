c9286cecb309bd01bea7f57197984dcf59d62f3b
Valentyn Valiaiev <valentine.valyaeff@gmail.com>
2025-02-07 20:57:38 +0200

    orb-ui: add green notch

--- ui/src/engine/pearl/self_serve.rs
+++ ui/src/engine/pearl/self_serve.rs
@@ -463,7 +463,7 @@ impl Runner<PEARL_RING_LED_COUNT, PEARL_CENTER_LED_COUNT> {
                 self.set_ring(
                     LEVEL_NOTICE,
                     animations::Alert::<PEARL_RING_LED_COUNT>::new(
-                        Argb::PEARL_RING_USER_CAPTURE,
+                        Argb::FULL_GREEN,
                         BlinkDurations::from(success_alert_blinks),
                         Some(vec![0.1, 0.4, 0.4, 0.2, 0.75, 0.2, 0.2, 1.0]),
                         false,
