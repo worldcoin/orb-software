34bf593924bbe4c53eb4ed5e039b7655461db31d
Ryan Butler <thebutlah@gmail.com>
2025-02-25 11:29:38 -0500

    tmp from plane

--- experiments/webtransport_video/src/video.rs
+++ experiments/webtransport_video/src/video.rs
@@ -5,6 +5,8 @@ use derive_more::{AsRef, Deref, Into};
 use tokio_util::sync::CancellationToken;
 use tracing::trace;
 
+use crate::EncodedPng;
+
 const WIDTH: usize = 640;
 const HEIGHT: usize = 480;
 const COLOR_TYPE: png::ColorType = png::ColorType::Rgb;
@@ -91,23 +93,6 @@ where
         let inner = Arc::into_inner(candidate.into())
             .expect("we just checked the reference count, should be infallible");
         out.push(inner);
-    }
-}
-
-/// Newtype on a vec, to indicate that this contains a png-encoded image.
-#[derive(Debug, Into, AsRef, Clone, Deref)]
-pub struct EncodedPng(pub Arc<Vec<u8>>);
-
-impl EncodedPng {
-    /// Equivalent to [`Self::clone`] but is more explicit that this operation is cheap.
-    pub fn clone_cheap(&self) -> Self {
-        EncodedPng(Arc::clone(&self.0))
-    }
-}
-
-impl AsRef<[u8]> for EncodedPng {
-    fn as_ref(&self) -> &[u8] {
-        self.0.as_slice()
     }
 }
 
