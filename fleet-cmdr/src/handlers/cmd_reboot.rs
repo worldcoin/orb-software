98f86defd76053b360cd65c97a6470ba3f042c86
Paul Quinn <paul.quinn@toolsforhumanity.com>
2025-02-27 16:30:06 -0800

    reboot command and test fixups

--- fleet-cmdr/src/handlers/cmd_reboot.rs
+++ fleet-cmdr/src/handlers/cmd_reboot.rs
@@ -2,7 +2,7 @@ use color_eyre::eyre::{Error, Result};
 use orb_relay_messages::fleet_cmdr::v1::{
     JobExecution, JobExecutionStatus, JobExecutionUpdate,
 };
-use tracing::info;
+use tracing::{error, info};
 
 #[derive(Debug)]
 pub struct OrbRebootCommandHandler {}
@@ -20,12 +20,36 @@ impl OrbRebootCommandHandler {
         job: &JobExecution,
     ) -> Result<JobExecutionUpdate, Error> {
         info!("Handling reboot command");
-        let response = JobExecutionUpdate {
-            job_id: job.job_id.clone(),
-            job_execution_id: job.job_execution_id.clone(),
-            status: JobExecutionStatus::Failed as i32,
-            std_out: "".to_string(),
-            std_err: "not implemented".to_string(),
+        let path = std::env::temp_dir().join("reboot.txt");
+        let response = if !path.exists() {
+            // I'm going to reboot.
+            info!("Rebooting orb due to job execution {}", job.job_execution_id);
+            std::fs::File::create(path).map_err(|e| {
+                error!("Failed to create reboot file: {}", e);
+                Error::new(e)
+            })?;
+            // TODO: Send dbus message to the orb to reboot.
+            JobExecutionUpdate {
+                job_id: job.job_id.clone(),
+                job_execution_id: job.job_execution_id.clone(),
+                status: JobExecutionStatus::Pending as i32,
+                std_out: "rebooting".to_string(),
+                std_err: "".to_string(),
+            }
+        } else {
+            // I'm back from a reboot.
+            info!("Orb rebooted due to job execution {}", job.job_execution_id);
+            std::fs::remove_file(path).map_err(|e| {
+                error!("Failed to remove reboot file: {}", e);
+                Error::new(e)
+            })?;
+            JobExecutionUpdate {
+                job_id: job.job_id.clone(),
+                job_execution_id: job.job_execution_id.clone(),
+                status: JobExecutionStatus::Completed as i32,
+                std_out: "rebooted".to_string(),
+                std_err: "".to_string(),
+            }
         };
         Ok(response)
     }
