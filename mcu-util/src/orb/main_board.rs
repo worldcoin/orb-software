ae00a3939c94376849234e05f04f3a1a05750ba6
Cyril Fougeray <cyril.fougeray@toolsforhumanity.com>
2025-03-10 13:05:18 +0100

    feat(mcu-util): control front leds
    add commands to control rgb and booster leds
    all leds are turned on for 3 seconds with the passed color

--- mcu-util/src/orb/main_board.rs
+++ mcu-util/src/orb/main_board.rs
@@ -9,7 +9,7 @@ use crate::orb::dfu::BlockIterator;
 use crate::orb::revision::OrbRevision;
 use crate::orb::{dfu, BatteryStatus};
 use crate::orb::{Board, OrbInfo};
-use crate::Camera;
+use crate::{Camera, Leds};
 use orb_mcu_interface::can::canfd::CanRawMessaging;
 use orb_mcu_interface::can::isotp::{CanIsoTpMessaging, IsoTpNodeIdentifier};
 use orb_mcu_interface::orb_messages;
@@ -226,6 +226,120 @@ impl MainBoard {
                     Err(e) => {
                         return Err(eyre!("Error enabling eye camera trigger: {:?}", e));
                     }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn front_leds(&mut self, leds: Leds) -> Result<()> {
+        if let Leds::Booster = leds {
+            match self
+                .isotp_iface
+                .send(McuPayload::ToMain(
+                    main_messaging::jetson_to_mcu::Payload::WhiteLedsBrightness(
+                        main_messaging::WhiteLeDsBrightness { brightness: 5 },
+                    ),
+                ))
+                .await
+            {
+                Ok(_) => {
+                    info!("🚀 Booster LEDs enabled");
+                }
+                Err(e) => {
+                    return Err(eyre!("Error enabling booster LEDs: {:?}", e));
+                }
+            }
+        } else {
+            let pattern = match leds {
+                Leds::Red => {
+                    main_messaging::user_le_ds_pattern::UserRgbLedPattern::AllRed
+                }
+                Leds::Green => {
+                    main_messaging::user_le_ds_pattern::UserRgbLedPattern::AllGreen
+                }
+                Leds::Blue => {
+                    main_messaging::user_le_ds_pattern::UserRgbLedPattern::AllBlue
+                }
+                Leds::White => {
+                    main_messaging::user_le_ds_pattern::UserRgbLedPattern::AllWhite
+                }
+                _ => {
+                    error!("Invalid rgb color");
+                    return Err(eyre!("Invalid LEDs"));
+                }
+            };
+
+            match self
+                .isotp_iface
+                .send(McuPayload::ToMain(
+                    main_messaging::jetson_to_mcu::Payload::UserLedsPattern(
+                        main_messaging::UserLeDsPattern {
+                            pattern: pattern as i32,
+                            custom_color: None,
+                            start_angle: 0,
+                            angle_length: 360,
+                            pulsing_scale: 0.0,
+                            pulsing_period_ms: 0,
+                        },
+                    ),
+                ))
+                .await
+            {
+                Ok(_) => {
+                    info!("🚦 {:?} enabled", pattern);
+                }
+                Err(e) => {
+                    return Err(eyre!("Error enabling green LEDs: {:?}", e));
+                }
+            }
+        }
+
+        // turn off all LEDs after 3 seconds
+        tokio::time::sleep(Duration::from_millis(3000)).await;
+
+        if let Leds::Booster = leds {
+            match self
+                .isotp_iface
+                .send(McuPayload::ToMain(
+                    main_messaging::jetson_to_mcu::Payload::WhiteLedsBrightness(
+                        main_messaging::WhiteLeDsBrightness { brightness: 0 },
+                    ),
+                ))
+                .await
+            {
+                Ok(_) => {
+                    info!("LEDs disabled");
+                }
+                Err(e) => {
+                    return Err(eyre!("Error disabling booster LEDs: {:?}", e));
+                }
+            }
+        } else {
+            match self
+                .isotp_iface
+                .send(McuPayload::ToMain(
+                    main_messaging::jetson_to_mcu::Payload::UserLedsPattern(
+                        main_messaging::UserLeDsPattern {
+                            pattern:
+                            main_messaging::user_le_ds_pattern::UserRgbLedPattern::Off
+                                as i32,
+                            custom_color: None,
+                            start_angle: 0,
+                            angle_length: 360,
+                            pulsing_scale: 0.0,
+                            pulsing_period_ms: 0,
+                        },
+                    ),
+                ))
+                .await
+            {
+                Ok(_) => {
+                    info!("LEDs disabled");
+                }
+                Err(e) => {
+                    return Err(eyre!("Error disabling RGB LEDs: {:?}", e));
                 }
             }
         }
