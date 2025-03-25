fe63efe686b2dd31a8d6a5f8395902cab6eaaea0
mpreibisch <michal.preibisch@toolsforhumanity.com>
2025-03-25 14:32:42 -0700

    Restore location directory to exact state from dade9e5236bcf9d95653c6c03965491bda065d68

---
+++ location/src/data.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+
+use crate::cell::{NeighborCell, ServingCell};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NetworkInfo {
+    pub wifi: Vec<WifiNetwork>,
+    pub cellular: CellularInfo,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct WifiNetwork {
+    pub bssid: String,
+    pub frequency: u32,
+    pub signal_level: i32,
+    pub flags: String,
+    pub ssid: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CellularInfo {
+    pub serving_cell: ServingCell,
+    pub neighbor_cells: Vec<NeighborCell>,
+}
---
+++ location/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod backend;
+pub mod cell;
+pub mod config;
+pub mod data;
+pub mod errors;
+pub mod network_manager;
+pub mod service;
+pub mod wifi;
+
+pub use errors::{LocationError, Result};
