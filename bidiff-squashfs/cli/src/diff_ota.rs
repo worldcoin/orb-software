1a434e3322b43acb17861bdf805e0d02303bc6dd
Ryan Butler <thebutlah@gmail.com>
2025-03-25 20:30:28 -0400

    fix(bidiff-squashfs): run destructors on ota subcommand (#441)

--- bidiff-squashfs/cli/src/diff_ota.rs
+++ bidiff-squashfs/cli/src/diff_ota.rs
@@ -1,9 +1,17 @@
 use color_eyre::Result;
 use std::path::Path;
 
+use tokio_util::sync::CancellationToken;
+
 /// Preconditions:
 /// - `out_dir` should be an empty directory.
-pub fn diff_ota(_base_dir: &Path, _top_dir: &Path, out_dir: &Path) -> Result<()> {
+pub fn diff_ota(
+    _base_dir: &Path,
+    _top_dir: &Path,
+    out_dir: &Path,
+    cancel: CancellationToken,
+) -> Result<()> {
+    let _cancel_guard = cancel.drop_guard();
     assert!(out_dir.is_dir(), "only directories should be provided");
     assert!(out_dir.exists(), "out_dir should exist");
 
