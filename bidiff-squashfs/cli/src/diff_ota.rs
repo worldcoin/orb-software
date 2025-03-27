ea05f3bea5974f50d4e96d12cd32f26782e6a6ae
Ryan Butler <thebutlah@gmail.com>
2025-03-27 17:58:00 -0400

    feat(bidiff-squashfs): implement claim patching (#450)

--- bidiff-squashfs/cli/src/diff_ota.rs
+++ bidiff-squashfs/cli/src/diff_ota.rs
@@ -5,7 +5,7 @@ use tokio::fs;
 use tokio_util::sync::CancellationToken;
 use tracing::info;
 
-use crate::diff_plan::{DiffPlan, OtaDir, OutDir};
+use crate::diff_plan::{patch_claim, DiffPlan, OtaDir, OutDir, PatchedClaim};
 
 /// Preconditions:
 /// - `out_dir` should be an empty directory.
@@ -24,17 +24,18 @@ pub async fn diff_ota(
         assert!(fs::metadata(d).await?.is_dir(), "{d:?} was not a directory");
     }
 
-    let plan = make_plan(base_dir, top_dir, out_dir)
+    let (plan, patched_claim) = make_plan(base_dir, top_dir, out_dir)
         .await
         .wrap_err("failed to create diffing plan")?;
     info!("created diffing plan: {plan:#?}");
+    info!("created patched claim: {patched_claim:#?}");
 
-    execute_plan(&plan)
+    execute_plan(plan, patched_claim)
         .await
         .wrap_err("failed to execute diffing plan")
 }
 
-async fn execute_plan(_plan: &DiffPlan) -> Result<()> {
+async fn execute_plan(_plan: DiffPlan, _patched_claim: PatchedClaim) -> Result<()> {
     todo!("plan execution")
 }
 
@@ -42,16 +43,17 @@ async fn make_plan(
     base_dir: &Path,
     top_dir: &Path,
     out_dir: &Path,
-) -> Result<DiffPlan> {
-    let old_ota = OtaDir::new(base_dir).await.wrap_err_with(|| {
+) -> Result<(DiffPlan, PatchedClaim)> {
+    let (old_ota, _old_claim) = OtaDir::new(base_dir).await.wrap_err_with(|| {
         format!("failed to validate OTA directory contents at {base_dir:?}")
     })?;
-    let new_ota = OtaDir::new(top_dir).await.wrap_err_with(|| {
+    let (new_ota, new_claim) = OtaDir::new(top_dir).await.wrap_err_with(|| {
         format!("failed to validate OTA directory contents at {top_dir:?}")
     })?;
     let out_dir = OutDir::new(out_dir);
 
     let plan = DiffPlan::new(&old_ota, &new_ota, &out_dir);
+    let patched_claim = patch_claim(&plan, &new_claim);
 
-    Ok(plan)
+    Ok((plan, patched_claim))
 }
