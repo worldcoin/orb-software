b92a9857602fcf927f900917d0dc19dce1754851
Panagiotis Karelis <panagiotis.karelis@toolsforhumanity.com>
2025-05-05 15:28:32 -0700

    const variables cleanup

--- ui/rgb/src/lib.rs
+++ ui/rgb/src/lib.rs
@@ -132,9 +132,6 @@ impl Argb {
     /// Outer-ring color during operator QR scans
     pub const DIAMOND_RING_OPERATOR_QR_SCAN: Argb = Argb(Some(5), 77, 14, 0);
     pub const DIAMOND_RING_OPERATOR_QR_SCAN_SPINNER: Argb = Argb(Some(10), 100, 88, 70);
-    pub const DIAMOND_RING_OPERATOR_QR_SCAN_SPINNER_OPERATOR_BASED: Argb =
-        Argb(Some(10), 100, 88, 20);
-    pub const DIAMOND_WAVE_MIN_COLOR_INTENSITY: Argb = Argb(Some(1), 1, 1, 1);
     /// Outer-ring color during user QR scans
     pub const DIAMOND_RING_USER_QR_SCAN: Argb = Argb(Some(10), 60, 80, 60);
     /// Shroud color to invite user to scan / reposition in front of the orb and capture
