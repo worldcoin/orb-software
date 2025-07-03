973bc32fabc62bd66f7bb052270f34ddbd3e7488
Ryan Butler <thebutlah@gmail.com>
2025-07-03 16:02:22 -0400

    switch to pipewire

--- nix/machines/hil-common.nix
+++ nix/machines/hil-common.nix
@@ -54,23 +54,19 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    pulse.enable = false; # Disable pipewire-pulse. IMO we don't need it.
+    wireplumber = {
+      enable = true;
+      configPackages = [
+      ];
+    };
   };
+  # redundant, here for clarity. This should be false when using sound servers
+  hardware.alsa.enable = false;
 
+  security.rtkit.enable = true;
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
