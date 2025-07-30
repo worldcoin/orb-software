fafc324fe5ef26a20aebad66be3a98247a199041
Popov Philipp <filipp.popov@toolsforhumanity.com>
2025-07-30 12:23:05 +0200

    feat(blob-transfer): start node with inmem store

--- orb-blob/blob-transfer/src/lib.rs
+++ orb-blob/blob-transfer/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{path::Path, sync::Arc};
+
+use color_eyre::Result;
+use iroh::{protocol::Router, Endpoint};
+use iroh_blobs::{store::mem::MemStore, ticket::BlobTicket, BlobsProtocol};
+
+pub struct BlobNode {
+    endpoint: Endpoint,
+    store: Arc<MemStore>,
+    _router: Router,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl BlobNode {
+    pub async fn start() -> Result<Self> {
+        let endpoint = Endpoint::builder().discovery_n0().bind().await?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let store = Arc::new(MemStore::new());
+
+        let blobs = BlobsProtocol::new(&store, endpoint.clone(), None);
+
+        let router = Router::builder(endpoint.clone())
+            .accept(iroh_blobs::ALPN, blobs)
+            .spawn();
+
+        Ok(Self {
+            endpoint,
+            store,
+            _router: router,
+        })
     }
 }
