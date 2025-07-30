be86ffb5cd120ecd351d4aada34805d55260327d
Ryan Butler <thebutlah@gmail.com>
2025-07-30 16:15:59 +0300

    feat(blob): tag and domain parsing

--- orb-blob/p2p/src/hash.rs
+++ orb-blob/p2p/src/hash.rs
@@ -1,1 +1,10 @@
 //! The hashes api
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct Hash(iroh_blobs::Hash);
+
+/// Topic for a blob's hash
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct HashTopic {
+    pub hash: Hash,
+}
--- orb-blob/p2p/src/lib.rs
+++ orb-blob/p2p/src/lib.rs
@@ -2,3 +2,13 @@
 
 mod hash;
 mod tag;
+
+pub use crate::hash::{Hash, HashTopic};
+pub use crate::tag::{Tag, TagTopic};
+
+/// Topic for a blob, addressible either by hash or by tag.
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub enum BlobTopic {
+    Hash(HashTopic),
+    Tag(TagTopic),
+}
