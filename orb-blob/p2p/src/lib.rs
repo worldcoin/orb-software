639e6d684596256efdb85dc4e57f5b09ca1a7e77
Ryan Butler <thebutlah@gmail.com>
2025-07-31 18:19:30 +0300

    feat(blob): broadcast blob pinnning by hash

--- orb-blob/p2p/src/lib.rs
+++ orb-blob/p2p/src/lib.rs
@@ -11,6 +11,7 @@ pub use crate::tag::{Tag, TagTopic};
 use async_stream::stream;
 use eyre::{Context, Result};
 use futures::StreamExt;
+use hash::HashGossipMsg;
 use iroh::NodeId;
 use iroh_gossip::api::{ApiError, GossipApi};
 use iroh_gossip::proto::TopicId;
@@ -44,11 +45,37 @@ enum GossipMsg {
 
 #[derive(Debug, bon::Builder, Clone)]
 pub struct Client {
+    my_node_id: NodeId, // Ideally we would figure this out directly from the underlying iroh
+    // endpoint internal to the GossipApi
     gossip: GossipApi,
     bootstrap_nodes: Vec<NodeId>,
 }
 
 impl Client {
+    pub async fn broadcast_to_peers(&self, topic: impl Into<BlobTopic>) -> Result<()> {
+        let blob_topic: BlobTopic = topic.into();
+        let topic_id = blob_topic.to_id();
+        let mut topic = self
+            .gossip
+            .subscribe_and_join(topic_id, self.bootstrap_nodes.clone())
+            .await
+            .wrap_err("failed to subscribe")?;
+
+        let broadcast_msg = match blob_topic {
+            BlobTopic::Tag(_) => todo!("tags are not yet supported"),
+            BlobTopic::Hash(_) => GossipMsg::Hash(HashGossipMsg {
+                node_id: self.my_node_id,
+            }),
+        };
+        let serialized = serde_json::to_vec(&broadcast_msg).expect("infallible");
+        topic
+            .broadcast(serialized.into())
+            .await
+            .wrap_err("failed to broadcast to peers")?;
+
+        Ok(())
+    }
+
     pub async fn listen_for_peers(
         &self,
         topic: impl Into<BlobTopic>,
