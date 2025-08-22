360f4ede8de32134b50df5ceedf7b1a5e4c8d7a1
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2025-08-22 14:25:31 +0200

    chore(connd): orb_telemetry

--- connd/src/main.rs
+++ connd/src/main.rs
@@ -1,8 +1,17 @@
 use color_eyre::eyre::Result;
 
+const SYSLOG_IDENTIFIER: &str = "worldcoin-connd";
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    orb_connd::run().await?;
+    color_eyre::install()?;
+    let tel_flusher = orb_telemetry::TelemetryConfig::new()
+        .with_journald(SYSLOG_IDENTIFIER)
+        .init();
 
-    Ok(())
+    let result = orb_connd::run().await;
+
+    tel_flusher.flush().await;
+
+    result
 }
