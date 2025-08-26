b21c0b6ab04aaeb9e52087f5204a24d57a85f32d
Danielle Nagar @ TFH <danielle.nagar@toolsforhumanity.com>
2025-08-27 01:14:29 +0700

    feat!: Add new major version for qr-link package (#655)
    ### Notes
    We are planning a new roll-out of the session validation flow:
    https://www.notion.so/worldcoin/Proposal-Eliminate-Session-Centric-Verification-Flow-2418614bdf8c802893d9c3fe43ebba81.
    
    This PR prepares a new version of qr codes where the orb knows the
    version of the QR code
    
    * A description of the new flow is written in lib
    * relies on the new orb relay message types specified in
    https://github.com/worldcoin/orb-relay-messages/pull/71

---
+++ qr-link/src/app_authenticated_data.rs
@@ -0,0 +1,59 @@
+use blake3::Hasher;
+use orb_relay_messages::common::v1::AppAuthenticatedData;
+
+// Define the constant needed by the implementation
+const PCP_VERSION_DEFAULT: u32 = 2;
+
+/// Extension trait for [`AppAuthenticatedData`] that provides hashing and verification functionality.
+///
+/// This trait adds methods to hash and verify the authenticity of [`AppAuthenticatedData`]
+/// instances using BLAKE3 cryptographic hashing.
+pub trait AppAuthenticatedDataExt {
+    /// Returns `true` if `hash` is a BLAKE3 hash of this [`AppAuthenticatedData`].
+    ///
+    /// This method calculates its own hash of the same length as the input
+    /// `hash` and checks if both hashes are identical.
+    fn verify(&self, hash: impl AsRef<[u8]>) -> bool;
+
+    /// Calculates a BLAKE3 hash of the length `n`.
+    fn hash(&self, n: usize) -> Vec<u8>;
+
+    /// Updates the provided BLAKE3 hasher with all fields of this [`AppAuthenticatedData`].
+    ///
+    /// This method must hash every field in the struct to ensure complete validation.
+    fn hasher_update(&self, hasher: &mut Hasher);
+}
+
+// Implement the trait for AppAuthenticatedData
+impl AppAuthenticatedDataExt for AppAuthenticatedData {
+    fn verify(&self, hash: impl AsRef<[u8]>) -> bool {
+        let external_hash = hash.as_ref();
+        let internal_hash = self.hash(external_hash.len());
+        external_hash == internal_hash
+    }
+
+    fn hash(&self, n: usize) -> Vec<u8> {
+        let mut hasher = Hasher::new();
+        self.hasher_update(&mut hasher);
+        let mut output = vec![0; n];
+        hasher.finalize_xof().fill(&mut output);
+        output
+    }
+
+    fn hasher_update(&self, hasher: &mut Hasher) {
+        let Self {
+            identity_commitment,
+            self_custody_public_key,
+            pcp_version,
+            os_version,
+            os,
+        } = self;
+        hasher.update(identity_commitment.as_bytes());
+        hasher.update(self_custody_public_key.as_bytes());
+        hasher.update(os_version.as_bytes());
+        hasher.update(os.as_bytes());
+        if *pcp_version != PCP_VERSION_DEFAULT {
+            hasher.update(&pcp_version.to_le_bytes());
+        }
+    }
+}
--- qr-link/tests/verification.rs
+++ qr-link/tests/verification.rs
@@ -1,47 +1,55 @@
-use orb_qr_link::{decode_qr, encode_qr, DataPolicy, UserData};
+use orb_qr_link::{decode_qr_with_version, encode_static_qr, AppAuthenticatedDataExt};
+use orb_relay_messages::common::v1::AppAuthenticatedData;
 use uuid::Uuid;
 
 #[test]
-fn test_encode_decode_verify_without_age_verification_token() {
-    let session_id = Uuid::new_v4();
+fn test_encode_decode() {
+    let orb_relay_id = Uuid::new_v4();
     let self_custody_public_key = r#"-----BEGIN PUBLIC KEY-----
 MCowBQYDK2VuAyEA2boNBmJX4lGkA9kjthS5crXOBxu2BPycKRMakpzgLG4=
 -----END PUBLIC KEY-----"#;
     let identity_commitment = "0xabcd";
-    let user_data = UserData {
+    let app_data = AppAuthenticatedData {
         identity_commitment: identity_commitment.to_string(),
         self_custody_public_key: self_custody_public_key.to_string(),
-        data_policy: DataPolicy::OptOut,
         pcp_version: 3,
-        user_centric_signup: true,
-        orb_relay_app_id: Some("123123".to_string()),
-        bypass_age_verification_token: None,
+        os: "Android".to_string(),
+        os_version: "1.2.3".to_string(),
     };
-    let qr = encode_qr(&session_id, user_data.hash(16));
-    let (parsed_session_id, parsed_user_data_hash) = decode_qr(&qr).unwrap();
-    assert_eq!(parsed_session_id, session_id);
-    assert!(user_data.verify(parsed_user_data_hash));
+    let hash_app_data = app_data.hash(16);
+    let qr = encode_static_qr(&orb_relay_id, hash_app_data);
+    let (version, parsed_orb_relay_id, parsed_app_data) =
+        decode_qr_with_version(&qr).unwrap();
+    assert_eq!(version, 4);
+    assert_eq!(parsed_orb_relay_id, orb_relay_id);
+    assert!(app_data.verify(parsed_app_data));
 }
-
 #[test]
-fn test_encode_decode_verify_with_age_verification_token() {
-    let session_id = Uuid::new_v4();
+fn test_encode_decode_failure() {
+    let orb_relay_id = Uuid::new_v4();
     let self_custody_public_key = r#"-----BEGIN PUBLIC KEY-----
 MCowBQYDK2VuAyEA2boNBmJX4lGkA9kjthS5crXOBxu2BPycKRMakpzgLG4=
 -----END PUBLIC KEY-----"#;
-    let sample_jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
     let identity_commitment = "0xabcd";
-    let user_data = UserData {
+    let app_data = AppAuthenticatedData {
         identity_commitment: identity_commitment.to_string(),
         self_custody_public_key: self_custody_public_key.to_string(),
-        data_policy: DataPolicy::OptOut,
         pcp_version: 3,
-        user_centric_signup: true,
-        orb_relay_app_id: Some("123123".to_string()),
-        bypass_age_verification_token: Some(sample_jwt_token.to_string()),
+        os: "Android".to_string(),
+        os_version: "1.2.3".to_string(),
     };
-    let qr = encode_qr(&session_id, user_data.hash(16));
-    let (parsed_session_id, parsed_user_data_hash) = decode_qr(&qr).unwrap();
-    assert_eq!(parsed_session_id, session_id);
-    assert!(user_data.verify(parsed_user_data_hash));
+    let hash_app_data = app_data.hash(16);
+    let qr = encode_static_qr(&orb_relay_id, hash_app_data);
+    let (version, parsed_orb_relay_id, parsed_app_data) =
+        decode_qr_with_version(&qr).unwrap();
+    assert_eq!(version, 4);
+    assert_eq!(parsed_orb_relay_id, orb_relay_id);
+    let incorrect_app_data = AppAuthenticatedData {
+        identity_commitment: "0x1234".to_string(),
+        self_custody_public_key: self_custody_public_key.to_string(),
+        pcp_version: 2,
+        os: "Android".to_string(),
+        os_version: "1.2.3".to_string(),
+    };
+    assert!(!incorrect_app_data.verify(parsed_app_data));
 }
