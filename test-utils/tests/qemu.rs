14be8b2942a954b470c41862365ac163fde626f6
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2025-09-17 21:50:51 +0200

    t-thanks Steve

--- test-utils/tests/qemu.rs
+++ test-utils/tests/qemu.rs
@@ -1,6 +1,7 @@
 use cmd_lib::run_cmd;
 use test_utils::qemu::{self, base};
 
+#[cfg_attr(target_os = "macos", test_with::no_env(GITHUB_ACTIONS))]
 #[test]
 fn run_in_parallel_one() {
     let q = qemu::fx::run("/tmp", &base::bullseye());
@@ -13,6 +14,7 @@ fn run_in_parallel_one() {
     assert_eq!(guest_hello, "blabla");
 }
 
+#[cfg_attr(target_os = "macos", test_with::no_env(GITHUB_ACTIONS))]
 #[test]
 fn run_in_parallel_two() {
     let q = qemu::fx::run("/tmp", &base::bullseye());
