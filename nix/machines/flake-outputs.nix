87064bcb0da16dcfc1e2a591f004751be8029019
Popov Philipp <filipp.popov@toolsforhumanity.com>
2025-10-21 13:27:18 +0200

    feat(hil): fix formatting

--- nix/machines/flake-outputs.nix
+++ nix/machines/flake-outputs.nix
@@ -86,7 +86,7 @@ in
   };
   nixosConfigurations."worldcoin-hil-munich-7" = hilConfig {
       hostname = "worldcoin-hil-munich-7";
-    };
+  };
   nixosConfigurations."worldcoin-hil-sf-0" = hilConfig {
     hostname = "worldcoin-hil-sf-0";
   };
