0cb58879a6ecb345fb31940a503305db047713ce
AlexKaravaev <alexkaravev@gmail.com>
2025-11-27 20:13:49 +0100

    fix: change to sudo reboot
    for some reason on hil orb mcu reboot doesn't work, before we conclude investigation we can use sudo reboot

--- hil/src/commands/ota/system.rs
+++ hil/src/commands/ota/system.rs
@@ -8,14 +8,9 @@ use serde_json::Value;
 /// Reboot the Orb device using orb-mcu-util and shutdown
 pub async fn reboot_orb(session: &SshWrapper) -> Result<()> {
     session
-        .execute_command("TERM=dumb orb-mcu-util reboot orb")
+        .execute_command("sudo reboot")
         .await
-        .wrap_err("Failed to execute orb-mcu-util reboot orb")?;
-
-    session
-        .execute_command("TERM=dumb sudo shutdown now")
-        .await
-        .wrap_err("Failed to execute shutdown now")?;
+        .wrap_err("Failed to execute sudo reboot")?;
 
     Ok(())
 }
