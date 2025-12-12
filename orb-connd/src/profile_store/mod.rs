4ec3aac0f86eeb6ea4c51cc4866f1345053fd4dc
Ryan Butler <thebutlah@gmail.com>
2025-12-12 18:00:06 -0500

    added basic implementation via secure storage

--- orb-connd/src/profile_store/mod.rs
+++ orb-connd/src/profile_store/mod.rs
@@ -1,59 +1,35 @@
 #![allow(dead_code)]
 
-use crate::{key_material::KeyMaterial, network_manager::WifiProfile};
-use chacha20poly1305::{aead::Aead, AeadCore, Key, KeyInit, XChaCha20Poly1305, XNonce};
-use color_eyre::{
-    eyre::{bail, ensure, eyre, Context},
-    Result,
-};
+use crate::network_manager::WifiProfile;
+use color_eyre::Result;
 use dashmap::{mapref::one::Ref, DashMap};
-use rand::rngs::OsRng;
-use secrecy::{ExposeSecret, SecretVec};
-use std::{collections::HashMap, path::PathBuf, sync::Arc};
-use tokio::fs;
+use std::{collections::HashMap, sync::Arc};
+
+use orb_secure_storage_ca::Client as SsClient;
+
+const STORAGE_KEY: &str = "nm-profiles-v1";
 
 pub struct ProfileStore {
-    key: Result<SecretVec<u8>>,
-    store_path: PathBuf,
     profiles: Arc<DashMap<String, WifiProfile>>,
+    client: SsClient, // TODO: Allow mocking
 }
 
 impl ProfileStore {
     const FILENAME: &str = "nmprofiles";
 
-    pub async fn from_store(
-        store_path: impl Into<PathBuf>,
-        key_material: &impl KeyMaterial,
-    ) -> Self {
-        Self {
-            key: key_material.fetch().await,
-            store_path: store_path.into(),
+    pub async fn new() -> Result<Self> {
+        Ok(Self {
             profiles: Arc::new(DashMap::new()),
-        }
+            client: SsClient::new()?,
+        })
     }
 
-    pub async fn import(&self) -> Result<()> {
-        let secret = match &self.key {
-            Err(e) => bail!("failed to retrieve key material: {e}"),
-            Ok(v) => v,
-        };
+    pub async fn import(&mut self) -> Result<()> {
+        // TODO: Handle changing euid to less priviledged user
 
-        let path = self.store_path.join(Self::FILENAME);
-        let mut bytes = fs::read(&path)
-            .await
-            .wrap_err_with(|| format!("failed to read profile store at {path:?}"))?;
+        let bytes = self.client.get(STORAGE_KEY)?;
 
-        ensure!(
-            bytes.len() >= 24,
-            "profile store is too short ({} bytes), should be at least 24 bytes",
-            bytes.len()
-        );
-
-        let contents = bytes.split_off(24);
-        let nonce = bytes;
-
-        let json = decrypt(contents, nonce, secret.expose_secret())?;
-        let profiles: HashMap<String, WifiProfile> = serde_json::from_slice(&json)?;
+        let profiles: HashMap<String, WifiProfile> = serde_json::from_slice(&bytes)?;
 
         for (key, value) in profiles {
             self.profiles.insert(key, value);
@@ -62,12 +38,8 @@ impl ProfileStore {
         Ok(())
     }
 
-    pub async fn commit(&self) -> Result<()> {
-        let secret = match &self.key {
-            Err(e) => bail!("failed to retrieve key material: {e}"),
-            Ok(v) => v,
-        };
-
+    pub async fn commit(&mut self) -> Result<()> {
+        // TODO: Handle changing euid to less priviledged user
         let profiles: HashMap<_, _> = self
             .profiles
             .iter()
@@ -75,10 +47,7 @@ impl ProfileStore {
             .collect();
 
         let json = serde_json::to_vec(&profiles)?;
-        let bytes = encrypt(json, secret.expose_secret())?;
-
-        let path = self.store_path.join(Self::FILENAME);
-        fs::write(path, bytes).await?;
+        self.client.put(STORAGE_KEY, &json)?;
 
         Ok(())
     }
@@ -98,54 +67,6 @@ impl ProfileStore {
     pub fn values(&self) -> Vec<WifiProfile> {
         self.profiles.iter().map(|x| x.value().clone()).collect()
     }
-}
-
-fn decrypt(bytes: Vec<u8>, mut nonce: Vec<u8>, secret: &[u8]) -> Result<Vec<u8>> {
-    ensure!(
-        nonce.len() == 24,
-        "none len must be 24 bytes, instead is {}",
-        nonce.len()
-    );
-
-    ensure!(
-        secret.len() == 32,
-        "secret len must be 32 bytes, instead is {}",
-        secret.len()
-    );
-
-    let nonce = XNonce::from_mut_slice(&mut nonce);
-
-    let secret = Key::from_slice(secret);
-    let cipher = XChaCha20Poly1305::new(secret);
-
-    let plaintext = cipher
-        .decrypt(nonce, bytes.as_slice())
-        .map_err(|e| eyre!("failed to decrypt profiles: {e:?}"))?;
-
-    Ok(plaintext)
-}
-
-fn encrypt(bytes: Vec<u8>, secret: &[u8]) -> Result<Vec<u8>> {
-    ensure!(
-        secret.len() == 32,
-        "secret len must be 32 bytes, instead is {}",
-        secret.len()
-    );
-
-    let mut rng = OsRng;
-    let nonce = XChaCha20Poly1305::generate_nonce(&mut rng);
-    let secret = Key::from_slice(secret);
-
-    let cipher = XChaCha20Poly1305::new(secret);
-    let ciphertext = cipher
-        .encrypt(&nonce, bytes.as_slice())
-        .map_err(|e| eyre!("failed to encrypt profiles: {e:?}"))?;
-
-    let mut out = Vec::new();
-    out.extend_from_slice(nonce.as_slice());
-    out.extend(ciphertext);
-
-    Ok(out)
 }
 
 #[cfg(test)]
