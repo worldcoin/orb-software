3c0e8861adf567e8b55daa7495c66a0f1badb2fd
Ryan Butler <thebutlah@gmail.com>
2026-01-13 14:28:22 -0500

    refactor(secure-storage): delete TA and move ca to toplevel

--- optee/secure-storage/ta/build.rs
+++
@@ -1,8 +0,0 @@
-use optee_utee_build::{Error, RustEdition, TaConfig};
-
-fn main() -> Result<(), Error> {
-    let config = TaConfig::new_default_with_cargo_env(
-        orb_secure_storage_proto::StorageDomain::WifiProfiles.as_uuid(),
-    )?;
-    optee_utee_build::build(RustEdition::Edition2024, config)
-}
