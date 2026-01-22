505ebb530cc8af85c83c2f2fee482f4785522f6c
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2026-01-22 13:08:34 +0100

    fix(jobs-agent): only force relay connectivity if we do in fact have a connection

--- orb-jobs-agent/src/conn_change.rs
+++ orb-jobs-agent/src/conn_change.rs
@@ -1,6 +1,8 @@
 use crate::job_system::client::JobClient;
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
+use orb_connd_events::ArchivedConnection;
 use orb_info::OrbId;
+use rkyv::AlignedVec;
 use tracing::info;
 
 /// forces relay reconnection every time there is a change to connectivity
@@ -16,8 +18,29 @@ pub async fn spawn_watcher(
 
     session
         .receiver(client)
-        .subscriber("connd/net/changed", async |client, _| {
-            info!("detected changed in connectivity, force relay reconnection");
+        .subscriber("connd/net/changed", async |client, sample| {
+            let mut bytes = AlignedVec::with_capacity(sample.payload().len());
+            bytes.extend_from_slice(&sample.payload().to_bytes());
+            let archived =
+                rkyv::check_archived_root::<orb_connd_events::Connection>(&bytes)
+                    .map_err(|e| eyre!("failed to deserialize Connection evt {e}"))?;
+
+            match archived {
+                ArchivedConnection::ConnectedGlobal(kind) => {
+                    info!(
+                        ?kind,
+                        "detected changed in connectivity, force relay reconnection"
+                    );
+                }
+
+                conn => {
+                    info!(
+                        ?conn,
+                        "detected changed in connectivity, but we have no global connectivity. doing nothing"
+                    );
+                }
+            };
+
             client.force_relay_reconnect().await?;
 
             Ok(())
