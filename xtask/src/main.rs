d4048220eccd9fbd47cbf174a75af595c42424b9
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2026-01-26 12:32:11 +0100

    chore(xtask): revert to not fully qualifying structs

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
 use color_eyre::Result;
-use x::cmd::{self, build, deb, deploy, pre_commit};
+use x::cmd::{build, deb, deploy, pre_commit};
 
 #[derive(Parser, Debug)]
 pub struct Cli {
@@ -12,10 +12,10 @@ pub struct Cli {
 enum Cmd {
     /// Build the select crate using `cargo zigbuild --release`. alias: 'b'
     #[command(alias = "b")]
-    Build(cmd::build::Args),
+    Build(build::Args),
     /// Build the select crate using `cargo zigbuild --release`, then package it into a `.deb` using
     /// `cargo deb`
-    Deb(cmd::deb::Args),
+    Deb(deb::Args),
     /// Lints and formats code. alias: 'pc'
     ///
     #[command(alias = "pc")]
@@ -23,7 +23,7 @@ enum Cmd {
     /// Builds a crate, packages it into a `.deb` and deploys it to an Orb. Automatically restarts
     /// any related systemd services.
     #[command(alias = "d")]
-    Deploy(cmd::deploy::Args),
+    Deploy(deploy::Args),
     #[command(subcommand)]
     Optee(orb_x_optee::Subcommands),
 }
