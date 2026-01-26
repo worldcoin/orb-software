3bfb2dcbbe5823f6831e52edc94230c17ba47ad6
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2026-01-26 11:41:17 +0000

    fix(xtask): stdout being captured by tracing subscriber (#958)
    ## fixes
    - `tracing_subscriber` capturing stdout of xtask tasks, making it hard
    to debug. moved it to the only command using `tracing` for logs
    
    ## reverts
    - unnecessary OOPfication of `Arg` structs

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
 use color_eyre::Result;
-use x::cmd;
+use x::cmd::{build, deb, deploy, pre_commit};
 
 #[derive(Parser, Debug)]
 pub struct Cli {
@@ -12,32 +12,34 @@ pub struct Cli {
 enum Cmd {
     /// Build the select crate using `cargo zigbuild --release`. alias: 'b'
     #[command(alias = "b")]
-    Build(cmd::build::Args),
+    Build(build::Args),
     /// Build the select crate using `cargo zigbuild --release`, then package it into a `.deb` using
     /// `cargo deb`
-    Deb(cmd::deb::Args),
+    Deb(deb::Args),
     /// Lints and formats code. alias: 'pc'
     ///
     #[command(alias = "pc")]
-    PreCommit(cmd::pre_commit::Args),
+    PreCommit,
     /// Builds a crate, packages it into a `.deb` and deploys it to an Orb. Automatically restarts
     /// any related systemd services.
     #[command(alias = "d")]
-    Deploy(cmd::deploy::Args),
+    Deploy(deploy::Args),
     #[command(subcommand)]
     Optee(orb_x_optee::Subcommands),
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    tracing_subscriber::fmt::init();
     let cmd = Cli::parse().subcmd;
 
     match cmd {
-        Cmd::Build(args) => args.run(),
-        Cmd::Deb(args) => args.run(),
-        Cmd::PreCommit(args) => args.run(),
-        Cmd::Deploy(args) => args.run(),
-        Cmd::Optee(args) => args.run(),
+        Cmd::Build(args) => build::run(args),
+        Cmd::Deb(args) => deb::run(args),
+        Cmd::PreCommit => pre_commit::run(),
+        Cmd::Deploy(args) => deploy::run(args),
+        Cmd::Optee(args) => {
+            tracing_subscriber::fmt::init();
+            args.run()
+        }
     }
 }
