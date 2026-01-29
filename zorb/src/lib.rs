26ee1a4c554c4ece42d6e68a5cc60c5457fa49f1
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2026-01-29 20:03:14 +0000

    feat(zorb): zenoh instrospection and conditional execution (#973)
    this introduces `zorb`, a CLI tool for Zenoh introspection and
    conditional command execution.
    
    [full README.md
    here](https://github.com/worldcoin/orb-software/blob/3740310bce4e88e7dc9fb077dbabe185bf5f641a/zorb/README.md)
    
    examples:
    ```bash
    # publishes on key expr test, payload "hello world" with TEXT_PLAIN encoding
    zorb pub test "hello world"
    
    # subscribes on test keyexpr, printing its contents every time an event is received
    zorb sub test
    
    # subscribes on connd/net/changed keyexpr, deserializing to given type and 
    # printing every time message is received
    zorb sub connd/net/changed -t orb_connd_events::Connection 
    
    # subscsribes on events/** keyexpr, decoding as plain text and executing 
    # the bash command given every time a message is received. %s% is a placeholder that is swapped with the payload of the event
    zorb when events/** echo %s% >> /tmp/events.log
    
    # same as above but using the -t flag to deserialize the event. in this case %s% 
    # is substituted with the Debug implementation of the deserialized type
    zorb when connd/net/changed -t orb_connd_events::Connection echo i just got %s%
    ```
    
    ---------
    
    Co-authored-by: Popov Philipp <filipp.popov@toolsforhumanity.com>

---
+++ zorb/src/lib.rs
@@ -0,0 +1,36 @@
+use rkyv::{bytecheck, Archive, CheckBytes, Deserialize, Serialize};
+
+pub type Handler = fn(&[u8]) -> color_eyre::Result<String>;
+
+#[macro_export]
+macro_rules! register_rkyv_types {
+    ($($ty:path),* $(,)?) => {{
+        let mut m: std::collections::HashMap<&'static str, $crate::Handler> = std::collections::HashMap::new();
+        $({
+            fn wrapper(bytes: &[u8]) -> color_eyre::Result<String> {
+                let mut aligned = rkyv::AlignedVec::with_capacity(bytes.len());
+                aligned.extend_from_slice(bytes);
+
+                let archived: &rkyv::Archived<$ty> =
+                    rkyv::check_archived_root::<$ty>(&aligned).map_err(|e| color_eyre::eyre::eyre!("{e}"))?;
+
+                let owned: $ty = rkyv::Deserialize::<$ty, rkyv::Infallible>::deserialize(
+                    archived,
+                    &mut rkyv::Infallible,
+                ).map_err(|e|color_eyre::eyre::eyre!("{e}"))?;
+
+                Ok(format!("{owned:?}"))
+            }
+
+            m.insert(stringify!($ty), wrapper as $crate::Handler);
+        })*
+        m
+    }};
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[archive_attr(derive(CheckBytes, Debug, PartialEq))]
+pub enum Example {
+    Foo,
+    Bar,
+}
