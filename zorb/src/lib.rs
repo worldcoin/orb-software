dcbf11f8caf9298ad4cf9645b92039524bdece60
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2026-01-29 18:44:58 +0100

    fix(zorb): rykv macro

--- zorb/src/lib.rs
+++ zorb/src/lib.rs
@@ -6,7 +6,7 @@ pub type Handler = fn(&[u8]) -> color_eyre::Result<String>;
 macro_rules! register_rkyv_types {
     ($($ty:path),* $(,)?) => {{
         let mut m: std::collections::HashMap<&'static str, $crate::Handler> = std::collections::HashMap::new();
-        $(
+        $({
             fn wrapper(bytes: &[u8]) -> color_eyre::Result<String> {
                 let archived: &rkyv::Archived<$ty> =
                     rkyv::check_archived_root::<$ty>(bytes).map_err(|e| color_eyre::eyre::eyre!("{e}"))?;
@@ -20,7 +20,7 @@ macro_rules! register_rkyv_types {
             }
 
             m.insert(stringify!($ty), wrapper as $crate::Handler);
-        )*
+        })*
         m
     }};
 }
