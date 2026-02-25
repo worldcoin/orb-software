ea05565cd706f8f527b57400372bcc86fdbb8582
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-02-25 11:23:09 +0100

    feat(hil): Refactor the orb-hil pin control (#1008)
    ### Refactor the pin control
    
    The key idea is to be able to use multiple types of pin control: FTDI,
    multiple types of relays. Hence, I'm decoupling all tasks controlling
    pins from FTDI. Also, this polymorphism has to be dynamic as we don't
    know the exact pin control type at compile-time. Also, this way the code
    becomes more testable (I'll add tests once I get the approval of the
    idea).
    
    In the end it will make HILs more stable, and will help to remove the
    configs of our HILs from the github of orb-os.
    
    Tested: reboots at my desk
    
    The next steps:
    - support configs from files
    - add the usb relay support (Christos has proved the capability)
    
    ---------
    
    Co-authored-by: chrisgalanis <christos.galanis@toolsforhumanity.com>

--- hil/src/commands/mod.rs
+++ hil/src/commands/mod.rs
@@ -19,3 +19,51 @@ pub use self::nfsboot::Nfsboot;
 pub use self::ota::Ota;
 pub use self::reboot::Reboot;
 pub use self::set_recovery_pin::SetRecoveryPin;
+
+use clap::Args;
+use color_eyre::{eyre::bail, Result};
+
+use crate::ftdi::FtdiGpio;
+use orb_hil::pin_controller::PinController;
+
+/// Parameters for creating a pin controller.
+#[derive(Debug, Clone, Args)]
+pub struct PinCtrl {
+    /// Type of pin controller to use (ftdi, relay, mock)
+    #[arg(long, default_value = "ftdi", value_name = "TYPE")]
+    pub pin_ctrl_type: String,
+
+    /// FTDI device serial number
+    #[arg(long)]
+    pub ftdi_serial_number: Option<String>,
+
+    /// FTDI device description
+    #[arg(long)]
+    pub ftdi_description: Option<String>,
+}
+
+impl PinCtrl {
+    pub fn build_controller(self) -> Result<Box<dyn PinController + Send>> {
+        match self.pin_ctrl_type.as_str() {
+            "ftdi" => {
+                let builder = FtdiGpio::builder();
+                let configured = match (self.ftdi_serial_number, self.ftdi_description)
+                {
+                    (Some(serial), _) => builder.with_serial_number(&serial)?,
+                    (None, Some(desc)) => builder.with_description(&desc)?,
+                    (None, None) => builder.with_default_device()?,
+                };
+                Ok(Box::new(configured.configure()?))
+            }
+            "relay" => {
+                bail!("Relay pin controller not yet implemented")
+            }
+            other => {
+                bail!(
+                    "Unknown pin controller type: '{}'. Supported types: ftdi, relay",
+                    other
+                )
+            }
+        }
+    }
+}
--- hil/src/commands/reboot.rs
+++ hil/src/commands/reboot.rs
@@ -1,31 +1,28 @@
 use clap::Parser;
 use color_eyre::{eyre::WrapErr as _, Result};
 
-use crate::ftdi::FtdiId;
+use crate::commands::PinCtrl;
 
 /// Reboot the orb
 #[derive(Debug, Parser)]
 pub struct Reboot {
     #[arg(short)]
     recovery: bool,
-    /// The serial number of the FTDI device to use
-    #[arg(long, conflicts_with = "desc")]
-    serial_num: Option<String>,
-    /// The description of the FTDI device to use
-    #[arg(long, conflicts_with = "serial_num")]
-    desc: Option<String>,
+    #[command(flatten)]
+    pin_ctrl: PinCtrl,
 }
 
 impl Reboot {
     pub async fn run(self) -> Result<()> {
-        let device = match (self.serial_num, self.desc) {
-            (Some(serial), None) => Some(FtdiId::SerialNumber(serial)),
-            (None, Some(desc)) => Some(FtdiId::Description(desc)),
-            (None, None) => None,
-            (Some(_), Some(_)) => unreachable!(),
-        };
+        let controller = tokio::task::spawn_blocking(move || {
+            self.pin_ctrl
+                .build_controller()
+                .wrap_err("failed to create pin controller")
+        })
+        .await
+        .wrap_err("task panicked")??;
 
-        crate::boot::reboot(self.recovery, device.as_ref())
+        crate::boot::reboot(self.recovery, controller)
             .await
             .wrap_err_with(|| {
                 format!(
