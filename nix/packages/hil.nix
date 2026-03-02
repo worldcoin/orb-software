77325ada703d91d2bbaabe10cfdc1bb11cca105c
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-03-02 18:39:46 +0100

    feat: install orb-hil onto our HILs (#1035)
    Have a tested orb-hil version installed on the HILs

--- nix/packages/hil.nix
+++ nix/packages/hil.nix
@@ -1,12 +1,16 @@
 # Packages for the HIL.
 { pkgs, ... }:
 with pkgs;
+let
+  orb-hil = pkgs.callPackage ./orb-hil.nix { };
+in
 [
   # HIL Specific
   awscli2
   cloudflared
   git
   gnutar
+  orb-hil
   picocom
   probe-rs
   ripgrep
