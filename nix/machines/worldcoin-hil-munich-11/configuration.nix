804ab645bce9f179ace84620a8ac0adea8fce36c
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-03-05 14:23:17 +0100

    chore(hil): add hil-11 configuration

--- nix/machines/worldcoin-hil-munich-11/configuration.nix
+++ nix/machines/worldcoin-hil-munich-11/configuration.nix
@@ -15,4 +15,19 @@
     ../nixos-common.nix
     ../hil-common.nix
   ];
+
+  environment.etc."worldcoin/orb.yaml" = {
+    text = ''
+      orb_id: 53d9f7ed
+      platform: pearl
+      # Pin controller configuration for orb-hil
+      # Type of pin controller to use (ftdi, relay)
+      pin_ctrl_type: usbrelay
+      serial_path: "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG031A17-if00-port0"
+      relay_bank: "/dev/hidraw0"
+      relay_power_channel: 2
+      relay_recovery_channel: 1
+    '';
+    mode = "0644";
+  };
 }
