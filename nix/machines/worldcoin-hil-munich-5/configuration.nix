05afa7fd926ef1acbd2a808e7dcb5dbf5ab0d216
chrisgalanis <50170911+chrisgalanis@users.noreply.github.com>
2026-03-16 18:32:42 +0100

    fix: Bump Orb HIL (#1078)
    This pull request updates the `orb-hil` package to a new version and
    ensures the correct checksum is used for the new release.
    
    Version and checksum update:
    
    * Updated the `version` field in `orb-hil.nix` from `"0.0.2-beta.16"` to
    `"0.0.2-beta.17"`, and replaced the `sha256` checksum to match the new
    release.

--- nix/machines/worldcoin-hil-munich-5/configuration.nix
+++ nix/machines/worldcoin-hil-munich-5/configuration.nix
@@ -24,7 +24,7 @@
       platform: ${config.worldcoin.orbPlatform}
       # Pin controller configuration for orb-hil
       # Type of pin controller to use (ftdi, relay)
-      pin_ctrl_type: ftdi
+      pin_ctrl_type: usb_relay
       serial_num: BG02MT7D
 
       serial_path: "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG02MT7D-if00-port0"
