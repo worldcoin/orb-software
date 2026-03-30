47a7a27b1580d18b2bb84b7acdfeb9cb9d480128
Ryan Butler <thebutlah@gmail.com>
2026-03-30 19:11:51 -0400

    fleshed out listener and harness more

--- se050-reprovision/tests/test_stuff.rs
+++ se050-reprovision/tests/test_stuff.rs
@@ -1,7 +1,22 @@
+use color_eyre::{eyre::WrapErr as _, Result};
+use rand::SeedableRng;
+
 mod harness;
 
-#[test]
-fn foobar() {
-    let harness = harness::Harness::builder().build();
-    assert!(false);
+#[tokio::test]
+async fn foobar() -> Result<()> {
+    let _ = color_eyre::install();
+    let mock_server = wiremock::MockServer::start().await;
+    // TODO: Add mocked endpoints
+
+    let harness = harness::Harness::builder()
+        .seed(1337)
+        .mocked_server(mock_server)
+        .build();
+    let cfg = harness.make_program_cfg();
+    orb_se050_reprovision::run(cfg)
+        .await
+        .wrap_err("program error")?;
+
+    Ok(())
 }
