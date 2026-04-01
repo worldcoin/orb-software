5265507098b4067caaeb444679f91493b40a7f98
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-04-01 16:17:10 +0200

    chore(hil): bump orb-hil to beta 23 (#1128)

--- nix/packages/orb-hil.nix
+++ nix/packages/orb-hil.nix
@@ -2,11 +2,11 @@
 { pkgs }:
 pkgs.stdenv.mkDerivation rec {
   pname = "orb-hil";
-  version = "0.0.2-beta.22";
+  version = "0.0.2-beta.23";
 
   src = pkgs.fetchurl {
     url = "https://github.com/worldcoin/orb-software/releases/download/orb-hil%2Fv${version}/orb-hil_x86_64";
-    sha256 = "sha256-+JqlhaU7q2wCpQV3UMDRBtxmrU2+Ync33iH/OpRiobY=";
+    sha256 = "sha256-l4DNHmjjPmfig6x5ABsSTRE+/AOkPsDsGpbVHJDTUJs=";
   };
 
   dontUnpack = true;
