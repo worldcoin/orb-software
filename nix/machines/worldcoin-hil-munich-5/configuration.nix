bcd663eee455f0b06b5f608c63a3095cbae18863
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-04-24 15:01:23 +0200

    chore(hil): add ssh config for the orbs

--- nix/machines/worldcoin-hil-munich-5/configuration.nix
+++ nix/machines/worldcoin-hil-munich-5/configuration.nix
@@ -16,11 +16,12 @@
     ../hil-common.nix
   ];
 
+  worldcoin.orbId = "a4947d00";
   worldcoin.orbPlatform = "pearl";
 
   environment.etc."worldcoin/orb.yaml" = {
     text = ''
-      orb_id: a4947d00
+      orb_id: ${config.worldcoin.orbId}
       platform: ${config.worldcoin.orbPlatform}
       # Pin controller configuration for orb-hil
       # Type of pin controller to use (ftdi, relay)
