4814f789cc249b4e67b8e7f68d0da0976636ea4d
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-05-15 14:24:17 +0200

    feat: common hil rts directory (#1212)

--- nix/machines/hil-common.nix
+++ nix/machines/hil-common.nix
@@ -211,13 +211,17 @@ in
     users.users.${ghRunnerUser} = {
       isNormalUser = true;
       description = "User for github actions runner";
-      homeMode = "0711";
       extraGroups = [
         "wheel"
         "plugdev"
         "dialout"
       ];
     };
+
+    systemd.tmpfiles.rules = [
+      "d /opt/worldcoin 0755 root root - -"
+      "d /opt/worldcoin/rts 0777 root root - -"
+    ];
     users.groups = {
       "${ghRunnerUser}" = {
         members = [ ghRunnerUser ];
@@ -345,7 +349,7 @@ in
         serviceOverrides = {
           Environment = ''"PATH=/run/wrappers/bin:/run/current-system/sw/bin"''; # fixes missing sudo
           # Override the NixOS github-runner module's UMask=0066 so artifacts
-          # downloaded into ~/rts are readable by the worldcoin user.
+          # downloaded into /opt/worldcoin/rts are readable by the worldcoin user.
           UMask = lib.mkForce "0022";
 
           # Undo NixOS sandboxing
