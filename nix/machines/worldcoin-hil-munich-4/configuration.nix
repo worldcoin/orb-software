b90ac8adfa6b133f2ed24a7cb3ff12282a7cb1b1
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-06-10 16:19:34 +0200

    fix: hil4 serial path (#1259)

--- nix/machines/worldcoin-hil-munich-4/configuration.nix
+++ nix/machines/worldcoin-hil-munich-4/configuration.nix
@@ -26,7 +26,7 @@
       # Pin controller configuration for orb-hil
       # Type of pin controller to use (ftdi, relay)
       pin_ctrl_type: usb_relay
-      serial_path: "usb-FTDI_FT232R_USB_UART_BG02N1MA-if00-port0"
+      serial_path: "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_BG02N1MA-if00-port0"
       relay_bank: "/dev/hidraw0"
       relay_power_channel: 2
       relay_recovery_channel: 1
