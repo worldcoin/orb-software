4dc80b523f7832141b7fb5dfbba4d55a0ff4d1f3
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2026-06-24 14:53:19 +0200

    chore(supervisor;zenorb): better logging

--- supervisor/src/startup.rs
+++ supervisor/src/startup.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use color_eyre::eyre::WrapErr as _;
 use futures::{future::TryFutureExt as _, FutureExt as _};
 use orb_info::orb_os_release::OrbOsRelease;
-use tracing::debug;
+use tracing::{debug, info};
 use zbus::{Connection, ConnectionBuilder};
 use zenorb::Zenorb;
 
@@ -108,7 +108,7 @@ impl Application {
             .await
             .map_err(Error::EstablishSystemConnection)?;
 
-        debug!(
+        info!(
             unique_bus_name = ?system_connection.unique_name(),
             "system dbus assigned unique bus name",
         );
@@ -137,7 +137,7 @@ impl Application {
         .await
         .map_err(Error::EstablishSessionConnection)?;
 
-        debug!(
+        info!(
             unique_bus_name = ?session_connection.unique_name(),
             "session dbus assigned unique bus name",
         );
