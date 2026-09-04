35a9d8186fcc33822856b3b998abe6005076f4e5
Aleksei Fedotov <aleksei.fedotov@toolsforhumanity.com>
2026-09-04 14:18:33 -0700

    chore: remove unused pubkeys

--- nix/machines/ssh-keys.nix
+++ nix/machines/ssh-keys.nix
@@ -1,12 +1,8 @@
 [
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQZCFz+udzltV3J3juvqOzVht86fpS1PwLtH6PpY1eD philolippo@gmail.com"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJnx35WTioopNCzkzz0S8Kv/rmgBZTDl7Bdyynzpkxy theodore.sfikas@toolsforhumanity.com"
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEoVo3BKge5tQuYpDuWKJaypdpfUuw4cq3/BYRFNovtj ryan.butler@Ryan-Butler.local"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOhklnZHdjM0VD82Z1naZaoeM3Lr9dbrsM0r+J9sHqN alex@hq-small"
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1voU6h9hkOCXHlLiKV95sivmvxcGDExu/zrgmFeN05 ryan.butler@ryan-wld-asahi"
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILfpbCy8aXDeE8Y9V7TnolS0XovgJLWv9XC4J9cRoEZL ryan.butler@ryan-wld-darter"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDTqIUP8YNVeIZeqJEqOVXxkk0GDNC/HrtXNDUi7YXqi christos.galanis@toolsforhumanity.com"
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJhIceAkzVz/D0+T71tVCfjZj3YNkAkqbFP2NMCp3QBI cyril.fougeray@Cyril-Fougeray.local"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKx6pwMug4h2zv5sZSzCtcK7TNGuxD57rt5ypoZdd1Qi athanasios.varis@toolsforhumanity.com"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFBgsmZAz9bwKxXHM6i8AiYzMufSgPbblbayxl7bq6MB vlad-agievich@vlad-agievich-thinkpad"
   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIX+tH7uNlFSO9DwU+axL35UalBTcoZxlh/l4BPjLDww aleksei.fedotov@toolsforhumanity.com"
