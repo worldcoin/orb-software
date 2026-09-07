ffe634c4ce3c245d5e67f69fcd90ec84f012d0fd
Vlad Agievich <uladzislau.ahiyevich@toolsforhumanity.com>
2026-09-07 13:12:44 +0200

    fix(hil): use /opt/worldcoin for a workdir (#1405)
    /var/lib didn't work out
    /opt/worldcoin works better

--- nix/machines/hil-common.nix
+++ nix/machines/hil-common.nix
@@ -287,8 +287,8 @@ in
       "d /opt/worldcoin 0755 root root - -"
       "d /opt/worldcoin/rts 0777 root root - -"
       "d /run/hil-agent 0777 root root - -"
-      "d /var/lib/github-runner 0755 root root - -"
-      "d /var/lib/github-runner/${hostname} 0755 ${ghRunnerUser} ${ghRunnerUser} - -"
+      "d /opt/worldcoin/github-runner-workdir 0755 root root - -"
+      "d /opt/worldcoin/github-runner-workdir/${hostname} 0755 ${ghRunnerUser} ${ghRunnerUser} - -"
     ];
     users.groups = {
       "${ghRunnerUser}" = {
@@ -413,7 +413,7 @@ in
         replace = true;
         user = ghRunnerUser;
         runnerGroup = "hardware-in-the-loop-server";
-        workDir = "/var/lib/github-runner/${hostname}";
+        workDir = "/opt/worldcoin/github-runner-workdir/${hostname}";
 
         serviceOverrides = {
           Environment = [
