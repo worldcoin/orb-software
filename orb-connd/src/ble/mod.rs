57a78dc44525a46f194fd23f2d155229d0cad5c3
AlexKaravaev <alexkaravev@gmail.com>
2026-09-09 17:49:18 -0700

    fix(connd): include Orb ID in BLE advertising name

--- orb-connd/src/ble/mod.rs
+++ orb-connd/src/ble/mod.rs
@@ -113,6 +113,7 @@ pub async fn advertiser(ctx: mini::Ctx<Args>) -> Result<()> {
 
                 let advertisement = Advertisement {
                     advertisement_type: Type::Broadcast,
+                    local_name: Some(format!("ORB-{}", ctx.zenoh.orb_id())),
                     service_data: service_data.clone(),
                     timeout: Some(Duration::ZERO),
                     ..Default::default()
