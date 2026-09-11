c1a6dec6490d5a7e217d823f1a3295a88e517455
Popov Philipp <filipp.popov@toolsforhumanity.com>
2026-09-11 16:31:08 +0200

    feat(oes): prepare structure

---
+++ orb-backend-status/src/collectors/linux/mod.rs
@@ -0,0 +1,23 @@
+pub mod connectivity;
+pub mod core_signups;
+pub mod front_als;
+pub mod hardware_states;
+pub mod net_stats;
+pub mod token;
+pub mod update_progress;
+
+use crate::{dbus::intf_impl::BackendStatusImpl, orb_event_stream::OrbEventStream};
+use connectivity::GlobalConnectivity;
+use hardware_states::HardwareState;
+use orb_messages::main::AmbientLight;
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::watch;
+
+#[derive(Clone)]
+pub(crate) struct ZenorbCtx {
+    pub backend_status: BackendStatusImpl,
+    pub connectivity_tx: watch::Sender<GlobalConnectivity>,
+    pub hardware_states: Arc<tokio::sync::Mutex<HashMap<String, HardwareState>>>,
+    pub front_als: Arc<tokio::sync::Mutex<Option<AmbientLight>>>,
+    pub oes: OrbEventStream,
+}
--- orb-backend-status/src/collectors/mod.rs
+++ orb-backend-status/src/collectors/mod.rs
@@ -1,24 +1,8 @@
-pub mod connectivity;
-pub mod core_signups;
-pub mod front_als;
-pub mod hardware_states;
-pub mod net_stats;
+pub mod linux;
 pub mod oes_collector;
-pub mod token;
-pub mod update_progress;
 
-use crate::{dbus::intf_impl::BackendStatusImpl, orb_event_stream::OrbEventStream};
-use connectivity::GlobalConnectivity;
-use hardware_states::HardwareState;
-use orb_messages::main::AmbientLight;
-use std::{collections::HashMap, sync::Arc};
-use tokio::sync::watch;
-
-#[derive(Clone)]
-pub(crate) struct ZenorbCtx {
-    pub backend_status: BackendStatusImpl,
-    pub connectivity_tx: watch::Sender<GlobalConnectivity>,
-    pub hardware_states: Arc<tokio::sync::Mutex<HashMap<String, HardwareState>>>,
-    pub front_als: Arc<tokio::sync::Mutex<Option<AmbientLight>>>,
-    pub oes: OrbEventStream,
-}
+pub(crate) use linux::ZenorbCtx;
+pub use linux::{
+    connectivity, core_signups, front_als, hardware_states, net_stats, token,
+    update_progress,
+};
